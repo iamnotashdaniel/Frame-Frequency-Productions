14f21bd4c6d7aec20ac306d473bbf5ea508dc85a
iamnotashdaniel <iamnotashdaniel@gmail.com>
2025-08-24 22:10:57 +0530

    feat: Switch CDN for React modules
    Replaced esm.sh with skypack.dev for React and React DOM imports in index.html. This change aims to improve module loading performance and reliability.

--- hooks/useOnScreen.ts
+++ hooks/useOnScreen.ts
@@ -1,7 +1,7 @@
 
 import { useState, useEffect, RefObject } from 'react';
 
-const useOnScreen = <T extends Element,>(ref: RefObject<T>, options: IntersectionObserverInit = { threshold: 0.1 }): boolean => {
+const useOnScreen = <T extends Element>(ref: RefObject<T>, options: IntersectionObserverInit = { threshold: 0.1 }): boolean => {
   const [isIntersecting, setIntersecting] = useState(false);
 
   useEffect(() => {
